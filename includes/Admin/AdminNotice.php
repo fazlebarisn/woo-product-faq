5a662d11fb31b416aeaf68849b500a8dd4957912
Fazle Bari <fazlebarisn@gmail.com>
2022-08-11 16:29:43 +0600

    add test admin notic

--- includes/Admin/AdminNotice.php
+++ includes/Admin/AdminNotice.php
@@ -1,10 +1,16 @@
 <?php
 
 namespace Woo\Faq\Admin;
-class AdminNotic
-{
-    public function __construct()
-    {
-        echo "Me too";
+
+class AdminNotice{
+    public function __construct(){
+        add_action( 'admin_notices', [$this ,'sample_admin_notice__error'] );
+    }
+
+    public function sample_admin_notice__error(){
+        $class = 'notice notice-error';
+        $message = __( 'Irks! An error has occurred.', 'sample-text-domain' );
+     
+        printf( '<div class="%1$s"><p>%2$s</p></div>', esc_attr( $class ), esc_html( $message ) );
     }
 }
