5656e9d664dbb3cb2337b4438664d501cdeadb9a
Fazle Bari <fazlebarisn@gmail.com>
2022-08-25 13:28:25 +0600

    admin notice added

--- includes/Admin/AdminNotice.php
+++ includes/Admin/AdminNotice.php
@@ -4,12 +4,15 @@ namespace Woo\Faq\Admin;
 
 class AdminNotice{
     public function __construct(){
-        //add_action( 'admin_notices', [$this ,'sample_admin_notice__error'] );
+
+        if ( !in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
+            add_action( 'admin_notices', [ $this, 'admin_notice_missing_main_plugin' ] );
+        } 
     }
 
-    public function sample_admin_notice__error(){
+    public function admin_notice_missing_main_plugin(){
         $class = 'notice notice-error';
-        $message = __( 'Irks! An error has occurred.', 'sample-text-domain' );
+        $message = __( "Product FAQ for WooCommerce Requires WooCommerce to be Activated", "woofaq" );
      
         printf( '<div class="%1$s"><p>%2$s</p></div>', esc_attr( $class ), esc_html( $message ) );
     }
