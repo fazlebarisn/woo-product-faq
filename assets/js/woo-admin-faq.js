d5ab19f8ad9b35eedf0ee718f41c953bf04eabeb
Fazle Bari <fazlebarisn@gmail.com>
2022-08-29 20:20:34 +0600

    remove add question number

--- assets/js/woo-admin-faq.js
+++ assets/js/woo-admin-faq.js
@@ -7,8 +7,8 @@
           
           var myElement = '<div class="options_group">';
           myElement += '<p class="form-field faq_' + myNum + '_field ">';
-          myElement += '<label for="faq_' + myNum + '">Question ' + myNum + '</label><input type="text" class="faq_input" name="faq[question][' + myNum + ']" id="faq_' + myNum + '" value="" placeholder="Add Question"> </p><p class="form-field faq_ans_' + myNum + '_field ">';
-          myElement += '<label for="faq_ans_' + myNum + '">Answer ' + myNum +  '</label><input type="text" class="faq_input" name="faq[answer][' + myNum + ']" id="faq_ans_' + myNum + '" value="" placeholder="Add Answer"> </p></div>';
+          myElement += '<label for="faq_' + myNum + '">Question' + '</label><input type="text" class="faq_input" name="faq[question][' + myNum + ']" id="faq_' + myNum + '" value="" placeholder="Add Question"> </p><p class="form-field faq_ans_' + myNum + '_field ">';
+          myElement += '<label for="faq_ans_' + myNum + '">Answer' + '</label><input type="text" class="faq_input" name="faq[answer][' + myNum + ']" id="faq_ans_' + myNum + '" value="" placeholder="Add Answer"> </p></div>';
 
           $('div.option-group-wrapper').append(myElement);
 
