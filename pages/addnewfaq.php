b0b57a54361ff7998783e92a839d7b3b644d34e4
Fazle Bari <fazlebarisn@hotmail.com>
2022-09-29 22:04:17 +0600

    not sure

--- pages/addnewfaq.php
+++ pages/addnewfaq.php
@@ -1,1 +1,6 @@
-hello
+<h1>Add New Faq Page Options</h1>
+<?php settings_errors() ?>
+<form method="post" action="options.php">
+    <button>Add New Group</button>
+    <?php submit_button() ?>
+</form>
