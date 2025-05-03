ef38af720f7a616844f4e685a9a5a936d1fe429c
Fazle Bari <fazlebarisn@gmail.com>
2025-05-03 14:47:07 +0600

    working

--- assets/js/woo-admin-faq.js
+++ assets/js/woo-admin-faq.js
@@ -30,5 +30,72 @@
       // Increment the counter for the next click
       faqCounter++;
     });
+
+    // Archive FAQ code start here
+    // Initialize the FAQ groups container
+    let groupIndex = 0;
+
+    // Add FAQ Group
+    $("#fbs-add-faq-group").on("click", function () {
+      const template = $("#fbs-faq-group-template")
+        .html()
+        .replace(/_INDEX_/g, groupIndex);
+      $("#faq-groups-container").append(template);
+      groupIndex++;
+    });
+
+    // Remove FAQ Group
+    $("#faq-groups-container").on(
+      "click",
+      ".fbs-archive-remove-faq-group",
+      function () {
+        $(this).closest(".fbs-faq-archive-group").remove();
+      }
+    );
+
+    // Add FAQ Item
+    $("#faq-groups-container").on(
+      "click",
+      ".fsb-archive-add-faq-item",
+      function () {
+        const $group = $(this).closest(".fbs-faq-archive-group");
+        const groupIndex = $group
+          .find("select.archive-type")
+          .attr("name")
+          .match(/\d+/)[0];
+        const template = $("#fbs-archive-faq-item-template")
+          .html()
+          .replace(/_GROUP_INDEX_/g, groupIndex);
+        $group.find(".fbs-archive-faq-items").append(template);
+      }
+    );
+
+    // Remove FAQ Item
+    $("#faq-groups-container").on(
+      "click",
+      ".fbs-archive-remove-faq-item",
+      function () {
+        $(this).closest(".fbs-archive-faq-item").remove();
+      }
+    );
+
+    // Show/hide archive term row
+    $("#faq-groups-container").on("change", "select.archive-type", function () {
+      const selected = $(this).val();
+      const $termRow = $(this).closest("table").find(".archive-term-row");
+      if (selected === "product_cat" || selected === "product_tag") {
+        $termRow.show();
+      } else {
+        $termRow.hide();
+      }
+    });
+
+    // 🚀 Show one FAQ group and one FAQ item by default
+    $("#fbs-add-faq-group").trigger("click");
+    setTimeout(function () {
+      $("#faq-groups-container .fsb-archive-add-faq-item")
+        .first()
+        .trigger("click");
+    }, 100);
   });
 })(jQuery);
